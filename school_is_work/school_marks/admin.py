f68de5b9e391746c0fc17631788514996298721b
ru-Spicker <ru.spicker@gmail.com>
2018-09-26 14:37:29 +0400

    Added model MarkCost

--- school_is_work/school_marks/admin.py
+++ school_is_work/school_marks/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Scholar, Discipline, Mark
+from .models import Scholar, Discipline, Mark, MarksCost
 
 admin.site.register(Scholar)
 admin.site.register(Discipline)
 admin.site.register(Mark)
+admin.site.register(MarksCost)
--- school_is_work/school_marks/models.py
+++ school_is_work/school_marks/models.py
@@ -13,8 +13,24 @@ class Scholar(models.Model):
 		return f'{self.second_name} {self.first_name} {self.third_name}'
 
 
+class MarksCost(models.Model):
+	'''Define cost each school mark'''
+	name = models.CharField('Название ценовой категории', max_length=30)
+	start_date = models.DateField('Действует с')
+	end_date = models.DateField('Действует до')
+	fine_cost = models.IntegerField('Отлично')
+	good = models.IntegerField('Хорошо')
+	satisfactory = models.IntegerField('Удовлетворительно')
+	unsatisfactory = models.IntegerField('Неудовлетворительно')
+	unacceptable = models.IntegerField('Неприемлемо')
+
+	def __str__(self):
+		return self.name
+
+
 class Discipline(models.Model):
 	name = models.CharField('Предмет', max_length=120)
+	cur_cost = models.ForeignKey(MarksCost, on_delete=models.PROTECT, null=True)
 
 	def __str__(self):
 		return self.name
@@ -24,8 +40,9 @@ class Mark(models.Model):
 	scholar = models.ForeignKey(Scholar, on_delete=models.CASCADE)
 	discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE)
 	date_of_mark = models.DateField('Дата')
-	volume = models.IntegerField('Оценка')
+	mark = models.IntegerField('Оценка')
+	cost = models.IntegerField('Цена')
 
 	def __str__(self):
-		return f'{self.date_of_mark} - {self.scholar.__str__()} - {self.discipline.__str__()} - {self.volume}'
+		return f'{self.date_of_mark} - {self.scholar.__str__()} - {self.discipline.__str__()} - {self.mark}'
 
