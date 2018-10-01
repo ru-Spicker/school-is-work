3f15c7a16a6f3be1317dd72e123abfdcc8d57657
ru-Spicker <ru.spicker@gmail.com>
2018-10-01 22:06:35 +0400

    Add table of marks in scholar_info view

--- school_is_work/school_marks/views.py
+++ school_is_work/school_marks/views.py
@@ -1,18 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpRequest, HttpResponse
-from .models import Scholar
+from .models import Scholar, Mark
+from django.forms.models import model_to_dict
 
 
 def index(request):
-	scholar_list = Scholar.objects.all()
-	response = '<br>'.join([scholar.first_name for scholar in scholar_list])
-	return HttpResponse('Список учащихся:<br>' + response)
+	scholar_list = get_list_or_404(Scholar.objects.all())
+	context = {'scholar_list': scholar_list}
+	return render(request, 'school_marks/index.html', context)
 
 
 def scholar_info(request, scholar_id):
-	scholar = Scholar.objects.get(pk=scholar_id)
-	response = f'Информация об учащемся: {scholar.first_name}'
-	return HttpResponse(response)
+	scholar = get_object_or_404(Scholar, pk=scholar_id)
+	marks_set = scholar.mark_set.order_by('-date_of_mark')
+	marks = [model_to_dict(mark) for mark in marks_set]
+	context = {'scholar': model_to_dict(scholar), 'marks': marks}
+	print(context)
+	return render(request, 'school_marks/scholar_info.html', context)
 
 
 def add_mark(request, scholar_id):
